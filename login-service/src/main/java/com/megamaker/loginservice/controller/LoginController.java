4f6fc96e1f96d489b44e27c6ec1af47734184404
ness727 <gnsdl727@naver.com>
2024-05-24 16:04:22 +0900

    테스트용 로그인 추가

--- login-service/src/main/java/com/megamaker/loginservice/controller/LoginController.java
+++ login-service/src/main/java/com/megamaker/loginservice/controller/LoginController.java
@@ -1,15 +1,24 @@
 package com.megamaker.loginservice.controller;
 
+import com.megamaker.loginservice.security.oauth2.CustomOAuth2User;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
 import java.net.URI;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,4 +35,19 @@ public class LoginController {
                 .build();
     }
 
+    @GetMapping("/login/dev")
+    public void devLogin(HttpServletResponse response) throws IOException {
+        String userId = "4ab483b0-c6b0-4584-8b50-9dd22ce2c558";
+        Key secretKey = new SecretKeySpec(Base64.getEncoder().encode(environment.getProperty("token.secret").getBytes()),
+                Jwts.SIG.HS256.key().build().getAlgorithm());
+
+        String token = Jwts.builder()
+                .subject(userId)
+                .expiration(new Date(System.currentTimeMillis() +
+                        Long.parseLong(environment.getProperty("token.expiration_time"))))
+                .signWith(secretKey)
+                .compact();
+
+        response.setHeader("Auth", token);
+    }
 }
